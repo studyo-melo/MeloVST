6b1f480a75b853f37bdd273b67ddef8c85c89df6
FlorianPadoa <florian.p@padoa-group.com>
2025-02-05 22:25:51 +0100

    Revert "feat: try to fix sample rate"
    This reverts commit 292c225e588842bf83d49d903f830af176a47b16.

--- Source/Debug/AudioAppPlayer.h
+++ Source/Debug/AudioAppPlayer.h
@@ -15,12 +15,12 @@
 #include "../Files/WavFileHandler.h"
 
 // Ces constantes sont celles que vous utilisez déjà
-#define SAMPLE_RATE 44100
+#define SAMPLE_RATE 48000
 #define BITRATE 64000
 #define NUM_CHANNELS 2
 #define BIT_DEPTH 16
 #define OPUS_FRAME_SIZE 20    // en millisecondes
-#define OPUS_SAMPLE_RATE 48000
+#define OPUS_SAMPLE_RATE 44100
 
 
 template <typename T>
